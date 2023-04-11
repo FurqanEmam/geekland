f92648442480b31fa81bf5cb08eb5252c2a9966a
FurqanEmam <furqanemam@gmail.com>
2023-04-12 03:26:32 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>this is the homepage for geekland</h1>
+        </div>
+    );
+};
+
+export default Home;
