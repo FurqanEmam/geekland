59fd997a84ebdf854dcde932a243f3ac37bfd01c
FurqanEmam <furqanemam@gmail.com>
2023-04-28 15:54:12 +0600

    navbar and icons are added & fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+
+
     return (
         <div className='header'>
+
+            <div onClick={() => setOpen(!open)} className='nav-icon'>
+                <span>{
+                    open === true
+                        ? <XMarkIcon class="icons" />
+                        : <Bars3Icon class="icons" />
+                }
+                </span>
+            </div>
+
             <nav className='navbar'>
+
                 <div className="title">
-                    <h1>GeekLand</h1>
+                    <h1><span className='geeks'>Geeks</span><span className='land'>Land</span></h1>
                 </div>
-                <div className="link">
-                    <Link to="/statistics">Statistics</Link>
-                    <Link to="/applied">Applied Jobs</Link>
-                    <Link to="/blog">Blog</Link>
+                <div className={`link ${open ? 'link-open' : 'link-close'}`}>
+                    <ul>
+                        <li>
+                            <Link to="/statistics">Statistics</Link>
+                        </li>
+                        <li>
+                            <Link to="/applied">Applied Jobs</Link>
+                        </li>
+                        <li>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                    </ul>
+                    <div className='nav-btn'>
+                        <button className='btn'>Start Applying</button>
+                    </div>
                 </div>
-                <div className='nav-btn'>
-                    <button className='btn'>Start Applying</button>
-                </div>
+
             </nav>
         </div>
     );
